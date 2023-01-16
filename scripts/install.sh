9089cda2ecc88774d6ab6981ed10ff723627abcf
shaoanianche <github@duanfei.org>
2023-01-16 14:05:40 +0800

    changes on Mon Jan 16 14:05:40 UTC 2023

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME="dotfiles"
+GIT_REPO = "https://github.com/shaonianche/dotfiles.git"
 SRC_INSTALL_HOME="$HOME/.$PROJECT_NAME"
 _SCRIPT_="$0"
+
+if test -d "$SRC_INSTALL_HOME"; then
+  rm -rf "$SRC_INSTALL_HOME"
+  git clone --depth 1 "$GIT_REPO" "$SRC_INSTALL_HOME"
+  rm -rf "$SRC_INSTALL_HOME/README.md"
+  rm -rf "$SRC_INSTALL_HOME/license"
+else
+  git clone --depth 1 "$GIT_REPO" "$SRC_INSTALL_HOME"
+fi
 
 detect_os() {
   current_system=$(uname)
@@ -23,26 +33,31 @@ create_soft_link() {
 
 list_dot_files() {
   local dir_path=$1
-  for file in $(find "$dir_path" -maxdepth 1 -name ".*" -type f ! -name ".DS_Store"); do
-    create_soft_link $SRC_INSTALL_HOME/$(basename "$file") $HOME
+  find "$dir_path" -maxdepth 1 -name ".*" -type f ! -name ".DS_Store" | while read -r file; do
+    create_soft_link "$SRC_INSTALL_HOME/$(basename "$file")" "$HOME"
   done
 }
 
 list_config() {
   local dir_path=$1
-  for folder in $(find "$dir_path" -type d); do
-    create_soft_link $SRC_INSTALL_HOME/.config/$(basename "$folder") $HOME/.config
+  find "$dir_path" -type d | while read -r folder; do
+    create_soft_link "$SRC_INSTALL_HOME/.config/$(basename "$folder")" "$HOME/.config"
   done
 }
 
 detect_os
-list_dot_files $SRC_INSTALL_HOME
-list_config $SRC_INSTALL_HOME/.config
+list_dot_files "$SRC_INSTALL_HOME"
+list_config "$SRC_INSTALL_HOME/.config"
 
 if [ $? -eq 0 ]; then
   echo "Current system is macOS"
-  create_soft_link $SRC_INSTALL_HOME/.config/zsh/.zshrc $HOME/.zshrc
-elif [ $? -eq 1 ]; then
+  create_soft_link "$SRC_INSTALL_HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
+  rm -rf "$SRC_INSTALL_HOME/windows"
+  rm -rf "$SRC_INSTALL_HOME/linux"
+  rm -rf "$SRC_INSTALL_HOME/.git"
+elif
+  [ $? -eq 1 ]
+then
   echo "Current system is Linux"
 else
   echo "Current system is not macOS or Linux"
