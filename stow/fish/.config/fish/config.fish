d14cf19d496ff436beccdd418d722a0d60927808
shaonianche <github@duanfei.org>
2025-05-14 15:54:24 +0800

    fix(config.fish): correct the path for virtual environment activation

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path "$HOME/.volta/bin"
+    set -gx GPG_TTY (tty)
+    set -gx VOLTA_HOME "$HOME/.volta"
     source "$HOME/.cargo/env.fish"
     source (starship init fish --print-full-init | psub)
     if test -f ~/.venv/bin/activate.fish
