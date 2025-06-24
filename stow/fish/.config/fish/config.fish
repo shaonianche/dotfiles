0e71bae43867ef1477f7fe676014fd6c7993a1e2
shaonianche <github@duanfei.org>
2025-06-24 23:58:51 +0800

    update
    Signed-off-by: shaonianche <github@duanfei.org>

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,11 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_add_path "$HOME/.volta/bin"
     set -gx GPG_TTY (tty)
     set -Ux EDITOR vim
     set -gx VOLTA_HOME "$HOME/.volta"
     source "$HOME/.cargo/env.fish"
-    source (starship init fish --print-full-init | psub)
+    starship init fish | source
     if test -f ~/.venv/bin/activate.fish
         source $HOME/.venv/bin/activate.fish
     end
@@ -34,3 +33,10 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# macports
+set -gx PATH "/opt/local/bin/" $PATH
+
+# opencode
+fish_add_path "$HOME/.opencode/bin"
+fish_add_path "$HOME/.volta/bin"
